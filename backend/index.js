77bda7a4871a0efa5c26ae0c5e49c837053fd084
Akash Surve <surveakash01@gmail.com>
2022-12-01 23:55:05 +0530

    login router created

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 import express from "express";
 import connection from "./config/connection.js";
 import cors from "cors";
+import LoginRouter from "./controller/Login.controller.js";
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/user", LoginRouter);
 app.get("/", async (req, res) => {
   res.send("hi");
 });
+
 app.listen(port, async () => {
   try {
     await connection();
